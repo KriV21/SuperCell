9732f1ed5f734242316b63dc244b73c3b4db7240
KriV21 <kriv@rarus.ru>
2016-11-26 13:29:49 +0300

    First commit

---
+++ test/TestSuperCell.dpr
@@ -0,0 +1,14 @@
+program TestSuperCell;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uTestSuperCell in 'uTestSuperCell.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
